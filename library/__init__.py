57c512bebe9c6ae429b60c3648d2a4f758bd1d3a
yohashinoio <shinodeveloper1@gmail.com>
2024-09-26 19:45:52 +0900

    Update to set root directory when building the standard library

--- library/__init__.py
+++ library/__init__.py
@@ -58,9 +58,9 @@ def install_standard_library(kaede_lib_dir):
     lib_files = [file for file in lib_files if file not in autoload_files]
     t1 = tempfile.NamedTemporaryFile()
     t2 = tempfile.NamedTemporaryFile()
-    subprocess.run(["cargo", "run", "--release", "--", "--no-autoload",
+    subprocess.run(["cargo", "run", "--release", "--", "--root-dir", os.path.join(kaede_lib_src_dir, "autoload"), "--no-autoload",
                    "-c", "-o", t1.name, *autoload_files]).check_returncode()
-    subprocess.run(["cargo", "run", "--release", "--",
+    subprocess.run(["cargo", "run", "--release", "--", "--root-dir", os.path.join(kaede_lib_src_dir, "std"),
                    "-c", "-o", t2.name, *lib_files]).check_returncode()
     subprocess.run(["gcc", "-shared", "-fPIC", "-o",
                    os.path.join(kaede_lib_dir, "libkd.so"), t1.name, t2.name]).check_returncode()
